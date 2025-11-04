55fd8f6828573b46f27991880b50c22aacb31b34
coder-rishav <aishwaraytiwary@gmail.com>
2025-11-05 00:34:41 +0530

    feat: added hide/show of subsections and delete button for sections

--- app/learn.tsx
+++ app/learn.tsx
@@ -1,9 +1,9 @@
-import { View, Text, FlatList, StyleSheet, Pressable } from "react-native";
-import { useState } from "react";
+import { View, Text, FlatList, StyleSheet, Pressable, Alert } from "react-native";
+import { useState, useEffect } from "react";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-const dummy_data = [
+let dummy_data = [
   {
     id: 0,
     name: "section 0",
@@ -62,8 +62,12 @@ const dummy_data = [
 ]
 
 const TaskCard = ({ idx, task, isdone }) => {
-  return <View style={{ backgroundColor: idx % 2 == 0 ? "#B2D8D8" : "#66B2B2", padding: 10}}>
-    <Text>{task} {idx}</Text>  
+  return <View style={{ backgroundColor: idx % 2 == 0 ? "#B2D8D8" : "#66B2B2", padding: 10, justifyContent: "space-between", flexDirection: "row", alignItems: "center"}}>
+    <Text style={{ fontSize: 15, marginLeft: 10, fontWeight: "bold", color: "teal", textDecorationLine: isdone ? 'line-through' : undefined}}>{task}</Text>
+    <Pressable style={{ backgroundColor: isdone ? "green": "#41A67E", borderRadius: 5, padding: 5, width: 75, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{color: "white", fontWeight: "bold"}}>
+        { isdone ? "Undone" : "Done" }
+      </Text></Pressable>
   </View>
 }
 
@@ -75,19 +79,54 @@ const TabsSubSectionElements = ({ data }) => {
   />
 }
 
-const TabSection = ({ data }) => {
+const TabSection = ({ data, setData }) => {
   return <FlatList 
     data={data}
     renderItem={({ item }) => (
           <View>  
-            <Pressable style={ styles.section }>
-              <Text style={{ fontSize: 20, fontWeight: "bold" }}>{ item.name[0].toUpperCase() + item.name.substring(1) }</Text>
-              <View style={{ flexDirection: "row", backgroundColor: "teal", borderRadius: 5, padding: 10, alignItems: "center"}}>
-                <Ionicons name={"add"} size={15} color={"white"}/>
-                <Text style={{ color: "white", marginLeft: 10}}>Add Skill</Text>
-              </View>  
-            </Pressable>
-            <TabsSubSectionElements data={item.data} />
+            <View style={ styles.section }>            
+              <View style={{ flexDirection: "row", alignItems: "center"}}>
+                <Pressable onPress={() => {
+                  setData(() => data.map((m_item) => {
+                    if (m_item.id == item.id) {
+                      m_item.hidden = !m_item.hidden
+                    }
+                    return m_item
+                  }))
+                }}>
+                  <Ionicons name={ item.hidden ? "eye-off-outline" : "eye-outline"} size={20} />
+                </Pressable>
+                <Text style={{ fontSize: 20, fontWeight: "bold", marginLeft: 15 }}>{ item.name[0].toUpperCase() + item.name.substring(1) }</Text>
+
+              </View>
+
+              <View style={{ flexDirection: "row", alignItems:"center"}}>
+                <Pressable style={{ flexDirection: "row", backgroundColor: "teal", borderRadius: 5, padding: 10, alignItems: "center", marginRight: 10}}>
+                  <Ionicons onPress={() => alert("Hello")} name={"add"} size={15} color={"white"}/>
+                  <Text style={{ color: "white", marginLeft: 10}}>Add Skill</Text>
+                </Pressable>
+                <Pressable onPress={() => {
+                  Alert.alert(
+                    'Delete Section',
+                    'Are you sure you want to proceed?',
+                    [
+                      {
+                        text: 'Cancel',
+                        onPress: () => {},
+                      },
+                      {
+                       text: 'OK',
+                       onPress: () => setData(data => data.filter(mdata => mdata.id != item.id)),
+                      },
+                    ],
+                    { cancelable: false }
+                  )
+                }}>
+                  <Ionicons name={"trash-outline"} size={25} color={"red"} />                    
+                </Pressable>
+              </View>
+            </View>
+            { item.hidden ?  <></> : <TabsSubSectionElements data={item.data} />}
           </View>  
       )
     }
@@ -96,6 +135,10 @@ const TabSection = ({ data }) => {
 }
 
 export default function LearnPage() {
+
+  useEffect(() => {
+    dummy_data = dummy_data.map((item) => item.hidden = true)
+  }, [])
 
   const [data, setData] = useState(dummy_data);
 
@@ -110,7 +153,8 @@ export default function LearnPage() {
         {id: 2, task: "task 3", isdone: false},
         {id: 3, task: "task 4", isdone: true},
         {id: 4, task: "task 5", isdone: false},
-      ]
+      ],
+      hidden: true
     }]);
   }
 
@@ -121,7 +165,7 @@ export default function LearnPage() {
       <Text style={ styles.lightText }>Add a new Section</Text>
       <Text></Text>
     </Pressable>
-    <TabSection data={data} />
+    <TabSection data={data} setData={setData}/>
   </SafeAreaView>
 }
 
